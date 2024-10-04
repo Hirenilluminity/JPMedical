1a8e52125243e3154315e820e36f21d2d172c69d
phiren53 <phiren53@gmail.com>
2024-10-04 18:11:26 +0530

    Template Binding and Added Login Page

--- JPMedical/App_Start/BundleConfig.cs
+++ JPMedical/App_Start/BundleConfig.cs
@@ -20,11 +20,34 @@ namespace JPMedical
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/js/jquery.min.js",
+                      "~/Scripts/js/bootstrap.min.js",
+                      "~/Scripts/js/owl.carousel.min.js",
+                      "~/Scripts/js/progressbar.js",
+                      "~/Scripts/js/isotope.pkgd.min.js",
+                      "~/Scripts/js/jquery.countTo.min.js",
+                      "~/Scripts/js/jquery.magnific-popup.min.js",
+                      "~/Scripts/js/wow.min.js",
+                      "~/Content/rev/js/rbtools.min.js",
+                      "~/Content/rev/js/rs6.min.js",
+                      "~/Scripts/js/rev-custom.js",
+                      "~/Scripts/js/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.min.css",
+                      "~/Content/rev/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css",
+                      "~/Content/rev/fonts/font-awesome/css/font-awesome.css",
+                      "~/Content/rev/css/rs6.css",
+                      "~/Content/css/owl.carousel.min.css",
+                      "~/Content/css/progressbar.css",
+                      "~/Content/css/animations.min.css",
+                      "~/Content/css/magnific-popup.min.css",
+                      "~/Content/fonts/font-awesome/css/all.min.css",
+                      "~/Content/fonts/flaticon/flaticon.css",
+                      "~/Content/css/ionicons.min.css",
+                      "~/Content/fonts/themify-icons/themify-icons.css",
+                      "~/Content/css/style.css",
+                      "~/Content/css/responsive.css"));
         }
     }
 }
